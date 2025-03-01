b5e44f73d4e0c2705aa345023917e54b8e0c55fe
phantomn <tmdvyr123@gmail.com>
2025-03-01 17:44:47 +0900

    Upgrading Tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  content: ['./pages/**/*.js', './components/**/*.js', './layouts/**/*.js', './lib/**/*.js'],
+  content: [
+    './pages/**/*.js',
+    './components/**/*.js',
+    './layouts/**/*.js',
+    './lib/**/*.js',
+    './css/**/*.css',
+  ],
   darkMode: 'class',
   theme: {
     extend: {
